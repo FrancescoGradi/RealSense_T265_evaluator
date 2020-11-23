0d138c9a9ed2776bd30185942270f837dc3e67de
FrancescoGradi <gradifrancesco@yahoo.it>
2020-11-23 13:37:39 +0100

    Dati completi di pose

--- saveTrajectory.py
+++ saveTrajectory.py
@@ -21,13 +21,10 @@ def saveTrajectory(filename='trajectory'):
             pose = frames.get_pose_frame()
             if pose:
                 data = pose.get_pose_data()
-                print('Rotation: ', data.rotation)
-                print('Tracker_confidence ', data.tracker_confidence)
-                print('')
                 trajectory.append([data.translation.x, data.translation.y, data.translation.z,
                                    data.velocity.x, data.velocity.y, data.velocity.z,
                                    data.acceleration.x, data.acceleration.y, data.acceleration.z,
-                                   data.rotation.qi, data.rotation.qj, data.rotation.qk, data.rotation.qr,
+                                   data.rotation.x, data.rotation.y, data.rotation.z, data.rotation.w,
                                    data.angular_velocity.x, data.angular_velocity.y, data.angular_velocity.z,
                                    data.angular_acceleration.x, data.angular_acceleration.y, data.angular_acceleration.z,
                                    data.tracker_confidence, data.mapper_confidence])
@@ -39,6 +36,6 @@ def saveTrajectory(filename='trajectory'):
         elapsed_time = end - start
         print("Time elapsed: " + str(elapsed_time))
 
-        np.savetxt("data/{}_{}.csv".format(filename, str(time.strftime("%H_%M_%S", time.gmtime(elapsed_time)))),
+        np.savetxt("../data/{}_{}.csv".format(filename, str(time.strftime("%H_%M_%S", time.gmtime(elapsed_time)))),
                    np.asarray(trajectory), delimiter=",")
         pipe.stop()
